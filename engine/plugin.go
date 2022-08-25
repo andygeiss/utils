e0c3ab1d7043686d148716c014908776dd5cc13b
Andreas Geiß <andreas.geiss@atruvia.de>
2022-08-25 11:06:54 +0200

    plugin cannot stop the engine

--- engine/plugin.go
+++ engine/plugin.go
@@ -4,4 +4,4 @@ import "github.com/andygeiss/utils/engine/entity"
 
 // Plugin is a function which handles a specific kind of functionality
 // by using an Entity Repository to gain access to the entities.
-type Plugin func(er entity.Repository) (state int)
+type Plugin func(er entity.Repository)
