b3a4ad3d58df2e36f114ae42eef0325a4ac1d913
Andreas Geiß <andreas.geiss@atruvia.de>
2022-08-25 10:52:21 +0200

    plugin added

---
+++ engine/plugin.go
@@ -0,0 +1,7 @@
+package engine
+
+import "github.com/andygeiss/utils/engine/entity"
+
+// Plugin is a function which handles a specific kind of functionality
+// by using an Entity Repository to gain access to the entities.
+type Plugin func(er entity.Repository) (state int)
