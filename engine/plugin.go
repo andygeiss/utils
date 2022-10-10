edc47d1ffa6faafca10ffc3459eab908f9bc826f
Andreas Geiß <xcb7231@inhouse.ad.in.gad.de>
2022-10-10 12:56:48 +0200

    refactoring

--- engine/plugin.go
+++ engine/plugin.go
@@ -1,6 +1,6 @@
 package engine
 
-import "github.com/andygeiss/utils/engine/entity"
+import "github.com/andygeiss/utils/entity"
 
 // Plugin is a function which handles a specific kind of functionality
 // by using an Entity Repository to gain access to the entities.
