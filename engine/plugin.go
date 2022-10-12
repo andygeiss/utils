922ad71e4d4a7b0b0f89e26d8eb92748c196ce77
Andreas Geiß <andreas.geiss@atruvia.de>
2022-10-12 09:33:03 +0200

    plugin removed

--- engine/plugin.go
+++
@@ -1,7 +0,0 @@
-package engine
-
-import "github.com/andygeiss/utils/entity"
-
-// Plugin is a function which handles a specific kind of functionality
-// by using an Entity Repository to gain access to the entities.
-type Plugin func(er entity.Repository)
